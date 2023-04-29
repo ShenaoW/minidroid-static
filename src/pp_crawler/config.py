4c15988aec2659716884dbbfbf414627e7983ec4
ShenaoW <shenaowang@foxmail.com>
2023-04-29 15:53:35 +0800

    update ppcrawler

---
+++ src/pp_crawler/config.py
@@ -0,0 +1,16 @@
+import os
+
+HEADER = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 NetType/WIFI MicroMessenger/6.8.0(0x16080000) MacWechat/3.7.1(0x13070114) XWEB/30419 Flue',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Dest': 'document',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh'
+}
+
+UIN = 'Nzg4NDczNDc1'
+KEY = 'aebbb56cd75e191ab20407e077f830f4508016bc18be0dedc191e0a30c8a3ddefad4f0a9b859d4a4cc7d552803d32564d4dcf0a5fdc1863a84a428a99a69420d98d0e47a51a775f618b6c82e7b68faf70d3725189acfbbc8b44788e8e820d023521d6eecf83e3eeb587187af660a6a92fc146aeeb7813243851774f855dfe760'
+WAP_SID2 = 'CIPN/PcCEnZ5X0hNMlpTN0dkZ0lWbmo3TGJzZV9sRW5kbUFJWmpwTkUtZkF5UmhXMkVpSnhRd19wOTdSc1lRLWxETVRWbS1HUVlRYzl2VVF4NmZHeWtqOHlwQ3JlVnV2VlBKaXkyRld3Z0E4YUhJbklGQmlJWG5CSUFBQX5+MKOisqIGOAxAlE4='
+SAVE_PATH = os.path.dirname(os.path.abspath('__file__')) + '/src/pp_crawler/privacy_policy/'
